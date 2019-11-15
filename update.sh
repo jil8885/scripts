5cf2dace7ce39d1be424a09cd48de986c7cc63b1
Jeongin Lee <jil8885@hanyang.ac.kr>
2019-11-15 09:13:26 +0900

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,5 @@
+sudo apt -y update
+sudo apt -y full-upgrade
+sudo apt-get -y install --install-recommends linux-generic-hwe-18.04 xserver-xorg-hwe-18.04 wget
+sudo add-apt-repository -y ppa:teejee2008/ppa
+sudo ukuu install --v5.3.11
