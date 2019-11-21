79731053b6d0898199539dc11439199bcd5d2cc6
Jeongin Lee <jil8885@hanyang.ac.kr>
2019-11-21 12:38:01 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,4 +2,5 @@ sudo apt -y update
 sudo apt -y full-upgrade
 sudo apt-get -y install --install-recommends linux-generic-hwe-18.04 xserver-xorg-hwe-18.04 wget
 sudo add-apt-repository -y ppa:teejee2008/ppa
+sudo apt install ukuu
 sudo ukuu --install v5.3.11
