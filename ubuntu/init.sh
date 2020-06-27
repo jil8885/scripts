310d72be5b9ff799fb05c0fd82f56480a5e06a82
Jeongin Lee <jil8885@hanyang.ac.kr>
2020-06-27 13:06:53 +0900

    update scripts

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -1,11 +1,18 @@
 sudo apt dist-upgrade
 # sudo apt-get -y install --install-recommends linux-generic-hwe-18.04 xserver-xorg-hwe-18.04
 sudo apt -y install wget curl build-essential pkg-config libsdl1.2-dev git zlib1g-dev liblzo2-dev liblzma-dev libfontconfig-dev libicu-dev libfluidsynth-dev
-sudo apt -y install fcitx fcitx-hangul snapd zsh wget git python3-pip openjdk-8-jdk ant
-im-config -n fcitx
+sudo apt -y install snapd zsh wget git python3-pip openjdk-8-jdk ant
 wget https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install/master/jetbrains-toolbox.sh
 chmod +x jetbrains-toolbox.sh
 ./jetbrains-toolbox.sh
 sudo sh -c 'curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/microsoft.gpg'
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get update && sudo apt-get install code
+
+wget -O - http://apt.hamonikr.org/hamonikr.key | sudo apt-key add -
+sudo bash -c "echo 'deb https://apt.hamonikr.org jin main upstream' > /etc/apt/sources.list.d/hamonikr-jin.list"
+sudo bash -c "echo 'deb-src https://apt.hamonikr.org jin main upstream' >> /etc/apt/sources.list.d/hamonikr-jin.list"
+
+sudo apt-get update
+sudo apt install nimf nimf-libhangul
+im-config -n nimf
--- ubuntu/package.sh
+++ ubuntu/package.sh
@@ -1,19 +1,22 @@
 sudo apt install build-essential pkg-config libsdl1.2-dev git zlib1g-dev liblzo2-dev liblzma-dev libfontconfig-dev libicu-dev libfluidsynth-dev
-sudo apt install fcitx fcitx-hangul snapd zsh wget git python3-pip openjdk-8-jdk ant language-pack-gnome-ko gnome-tweak-tool
-im-config -n fcitx
-sudo sh -c 'echo "deb http://repo.whale.naver.com/linux/ stable/" >> /etc/apt/sources.list.d/naver-whale.list'
-wget -q -O - http://repo.whale.naver.com/linux/stable/public.key | sudo apt-key add -
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt update
-sudo apt install naver-whale-stable
-sudo apt install google-chrome-stable
+sudo apt install snapd zsh wget git python3-pip openjdk-8-jdk ant language-pack-gnome-ko gnome-tweak-tool
+# im-config -n fcitx
+# sudo sh -c 'echo "deb http://repo.whale.naver.com/linux/ stable/" >> /etc/apt/sources.list.d/naver-whale.list'
+# wget -q -O - http://repo.whale.naver.com/linux/stable/public.key | sudo apt-key add -
+# wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+# sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+# sudo apt update
+# sudo apt install naver-whale-stable
+# sudo apt install google-chrome-stable
 # sudo rm -rf /etc/apt/sources.list.d/google.list
 # sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
 # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 # sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 #sudo apt update 
 # sudo apt -y install docker-ce
-wget https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install/master/jetbrains-toolbox.sh
-chmod +x jetbrains-toolbox.sh
-./jetbrains-toolbox.sh
+# wget https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install/master/jetbrains-toolbox.sh
+# chmod +x jetbrains-toolbox.sh
+# ./jetbrains-toolbox.sh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
