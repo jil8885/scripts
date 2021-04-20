569e034abcdb06f66a531ce3669c0cd18d0c6fb8
Jeongin Lee <jil8885@hanyang.ac.kr>
2021-04-20 09:03:23 +0900

    Update arch script for RX5077

--- arch/install.sh
+++ arch/install.sh
@@ -1,11 +1,11 @@
-mkfs.ext4 /dev/sda3
-mkswap /dev/sda5
-swapon /dev/sda5
-mount /dev/sda3 /mnt
+mkfs.ext4 /dev/sda1
+mkswap /dev/sda2
+swapon /dev/sda2
+mount /dev/nvmen0p1 /mnt
 mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
+mount /dev/ /mnt/boot
 echo Server=http://ftp.lanet.kr/pub/archlinux/$repo/os/$arch > /etc/pacman.d/mirrorlist
 echo Server=http://ftp.harukasan.org/archlinux/$repo/os/$arch >> /etc/pacman.d/mirrorlist
-pacstrap /mnt base linux linux-firmware
+pacstrap /mnt base linux linux-firmware nano networkmanager base-devel
 getfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
--- arch/systemset.sh
+++ arch/systemset.sh
@@ -1,18 +1,22 @@
 passwd
-pacman -S nano
-nano /etc/local.gen
+nano -w /etc/local.gen
 export LANG=en_US.UTF-8 > /etc/locale.conf
 echo jeongin-laptop > /etc/hostname
-ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
+ntpd -q -g
 hwclock --systohc --utc
+ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
 pacman -S zsh sudo
 useradd -m -G users,wheel -s /bin/zsh jil8885
 passwd jil8885
-nano /etc/sudoers
-pacman -S grub efibootmgr networkmanager
+EDITOR=nano visudo
+visudo /etc/sudoers
+pacman -S grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch --recheck
-pacman -S intel-ucode mesa
+pacman -S amd-ucode nvidia
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
+pacman -S gnome
+systemctl enable gdm
+nvidia-xconfig
 exit
 
