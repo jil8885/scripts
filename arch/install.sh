c4b32f8b38d49e824a6831e9d8849768d2c2e963
Jeongin Lee <jil8885@hanyang.ac.kr>
2021-04-20 09:15:53 +0000

    fix wrong commands

--- arch/install.sh
+++ arch/install.sh
@@ -1,11 +1,11 @@
 mkfs.ext4 /dev/sda1
 mkswap /dev/sda2
 swapon /dev/sda2
-mount /dev/nvmen0p1 /mnt
+mount /dev/sda1 /mnt
 mkdir /mnt/boot
-mount /dev/ /mnt/boot
-echo Server=http://ftp.lanet.kr/pub/archlinux/$repo/os/$arch > /etc/pacman.d/mirrorlist
-echo Server=http://ftp.harukasan.org/archlinux/$repo/os/$arch >> /etc/pacman.d/mirrorlist
+mount /dev/nvmen0p1 /mnt/boot
+echo Server='http://ftp.lanet.kr/pub/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
+echo Server='http://ftp.harukasan.org/archlinux/$repo/os/$arch' >> /etc/pacman.d/mirrorlist
 pacstrap /mnt base linux linux-firmware nano networkmanager base-devel
-getfstab -U /mnt >> /mnt/etc/fstab
+genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
